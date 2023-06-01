74c5d4afa6a0b04865755065a2e99e15150df61d
AliMoradpour <mr.mrdp@yahoo.com>
2023-06-01 15:12:01 +0430

    complete color picker tab

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -18,6 +18,27 @@ import {
 
 const Customizer = () => {
   const snap = useSnapshot(state);
+  const [file, setFile] = useState("");
+  const [prompt, setPrompt] = useState("");
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [activeEditorTab, setActiveEditorTab] = useState("second");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false,
+  });
+
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case "colorpicker":
+        return <ColorPicker />;
+      case "filepicker":
+        return <FilePicker />;
+      case "aipicker":
+        return <AIPicker />;
+      default:
+        return null;
+    }
+  };
 
   return (
     <AnimatePresence>
@@ -30,8 +51,9 @@ const Customizer = () => {
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
                 {EditorTabs.map((tab) => (
-                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                  <Tab key={tab.name} tab={tab} handleClick={() => setActiveEditorTab(tab.name)} />
                 ))}
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
@@ -42,15 +64,23 @@ const Customizer = () => {
             <CustomButton
               type="filled"
               title="Go Back"
-              handleClick={() => state.intro = true}
+              handleClick={() => (state.intro = true)}
               customStyles="w-fit px-4 py-2.5 font-bold text-sm"
             />
           </motion.div>
 
-          <motion.div className="filtertabs-container" {...slideAnimation('up')}>
-          {FilterTabs.map((tab) => (
-                  <Tab key={tab.name} tab={tab} isFilterTab isActiveTab="" handleClick={() => {}} />
-                ))}
+          <motion.div
+            className="filtertabs-container"
+            {...slideAnimation("up")}>
+            {FilterTabs.map((tab) => (
+              <Tab
+                key={tab.name}
+                tab={tab}
+                isFilterTab
+                isActiveTab=""
+                handleClick={() => {}}
+              />
+            ))}
           </motion.div>
         </>
       )}
